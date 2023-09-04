d62ec255f3ad4307feb1e6ea16c18922aea7800d
Thw <chanhosatoh@gmail.com>
2023-09-05 02:28:58 +0700

    cuccess gui

--- Cache/__init__.py
+++ Cache/__init__.py
@@ -1,3 +1,57 @@
+from typing import Union
+from collections.abc import Callable
+from pathlib import Path
+import pickle
+import os
+
+
 class Cache:
-    def __init__(self):
-        pass
+    def __init__(self, obj: object, name: str) -> None:
+        cache_dir = Path.home()/("."+name)
+        if not os.path.isdir(cache_dir):
+            os.makedirs(cache_dir)
+        self._path = os.path.join(cache_dir, name+".cache")
+        if not os.path.isfile(self._path):
+            with open(self._path, "wb") as f:
+                pickle.dump({}, f, pickle.HIGHEST_PROTOCOL)
+        self._data = {}
+        self._obj = obj
+        self._error = False
+        if "__cache__" not in dir(obj):
+            self._error = True
+            print("__cache__ method not found!")
+        else:
+            for cache_obj in self._obj.__cache__():
+                self._data[cache_obj.id] = cache_obj
+    
+    def load(self) -> dict:
+        if self._error:
+            return
+        with open(self._path, "rb") as f:
+            cache = pickle.load(f)
+        for _id in cache.keys():
+            if _id in self._data:
+                self._data[_id].set(cache[_id])
+        return cache
+    
+    def update(self) -> None:
+        cache = {}
+        for _id in self._data.keys():
+            cache[_id] = self._data[_id].get()
+        with open(self._path, "wb") as f:
+            pickle.dump(cache, f, pickle.HIGHEST_PROTOCOL)
+
+
+
+class CacheObject:
+    def __init__(self, _id: str, getter: Callable, setter: Callable, default: Union[str, int, float]) -> None:
+        self.id = _id
+        self.getter = getter
+        self.setter = setter
+        self.default = default
+    
+    def get(self) -> Union[str, int, float]:
+        return self.getter()
+
+    def set(self, value=None) -> None:
+        return self.setter(value if value is not None else self.default)
--- main.py
+++ main.py
@@ -1,14 +1,64 @@
 import sys
+import os
 import module
-from PyQt5.QtWidgets import QApplication, QMainWindow, QProgressBar, QSizePolicy, QFrame, QVBoxLayout
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QProgressBar, QFrame, QVBoxLayout, QMessageBox
+from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtGui import QCloseEvent
 from PyQt5.uic import loadUi
+from Cache import Cache, CacheObject
+from DHConstructionSite import ConstructionSiteReport
+import json
+
+
+class Worker(QThread):
+
+    create_one = pyqtSignal()
+    end = pyqtSignal()
+    error = pyqtSignal(str)
+
+    def __init__(self, file_name: str, output_dir: str, data: dict, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.file_name = file_name
+        self.output_dir = output_dir
+        self.data = data
+    
+    @staticmethod
+    def change_format(inp):
+        return "%sh%sp" % tuple(inp.split(":"))
+
+    def run(self) -> None:
+        try:
+            report = ConstructionSiteReport(self.file_name)
+            for page in self.data:
+                report.add_page(page["date"], page["name"], page["id"], 
+                                self.change_format(page["smo"]), self.change_format(page["emo"]), 
+                                self.change_format(page["saf"]), self.change_format(page["eaf"]), 
+                                page["jmo"], page["jaf"])
+                self.create_one.emit()
+            report.save(self.output_dir)
+        except PermissionError:
+            self.error.emit("Permission denied! Maybe the file is open")
+        except Exception as msg:
+            self.error.emit(msg)
+        else:
+            self.end.emit()
+
+    def stop(self) -> None:
+        self.terminate()
 
 
 class MainWindow(QMainWindow):
 
+    document = os.path.join(os.environ['USERPROFILE'], 'Documents')
     source = module.Path(__file__).source
     app = module.Path(__file__).app
+    
+    def __cache__(self) -> [CacheObject]:
+        return [
+            CacheObject("name", self.line_name.text, self.line_name.setText, ""),
+            CacheObject("input", self.line_input.text, self.line_input.setText, ""),
+            CacheObject("output", self.line_output.text, self.line_output.setText, "")
+        ]
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
@@ -16,27 +66,79 @@ class MainWindow(QMainWindow):
         self.line_input.setFilter("Text File (*.txt) ;; All (*.*)")
         self.line_input.setButton(self.btn_input)
         self.line_output.setButton(self.btn_output)
+        self.line_output.setPlaceholderText(self.document)
         self.btn_create.clicked.connect(self._on_btn_create_clicked)
+        self.__cache = Cache(self, "ConstructionSite")
+        self.__cache.load()
+        self._thread = None
+        self.progress = None
         self.show()
-
-    def _on_btn_create_clicked(self) -> None:
-        if not self.line_input.text():
-            self.alert_input.setText("input must not be empty")
-            return
-        with open(self.line_input.text(), mode="r", encoding="utf-8") as f:
-            data = f.read()
-        lst_data = data.split("\n\n\n")
+    
+    def create_progressbar(self) -> QProgressBar:
         self.btn_create.hide()
+        if self.progress:
+            self.progress.setValue(0)
+            self.frame.show()
+            return self.progress
         self.frame = QFrame(self.centralwidget)
         self.frame.setFrameShape(QFrame.StyledPanel)
         self.frame.setFrameShadow(QFrame.Raised)
         self.verticalLayout = QVBoxLayout(self.frame)
         self.progress = QProgressBar(self.frame)
         self.progress.setValue(0)
-        self.progress.setMaximum(len(lst_data))
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout.addWidget(self.progress)
-        self.gridLayout.addWidget(self.frame, 3, 1, 1, 1)
+        self.gridLayout.addWidget(self.frame, 5, 2, 1, 1)
+        return self.progress
+    
+    def get_input(self) -> (str, str, str):
+        file_name = self.line_name.text()
+        input_file = self.line_input.text()
+        output_dir = self.line_output.text()
+        if not output_dir:
+            output_dir = self.document
+        if not input_file:
+            self.alert_input.setText("Input must not be empty!")
+        elif not os.path.isfile(input_file):
+            self.alert_input.setText("Input invalid!")
+            input_file = ""
+        if not os.path.isdir(output_dir):
+            self.alert_output.setText("Output invalid!")
+            input_file = ""
+        return file_name, input_file, output_dir
+
+
+    def _on_btn_create_clicked(self) -> None:
+        file_name, input_file, output_dir = self.get_input()
+        if not input_file:
+            return
+        with open(input_file, mode="r", encoding="utf-8") as f:
+            data = json.loads(f.read())
+        self.create_progressbar().setMaximum(len(data))
+        self._thread = Worker(file_name, output_dir, data, self)
+        self._thread.create_one.connect(self._on_add_page)
+        self._thread.error.connect(self._on_end_create)
+        self._thread.end.connect(self._on_end_create)
+        self._thread.start()
+
+    def _on_add_page(self) -> None:
+        self.progress.setValue(self.progress.value() + 1)
+    
+    def _on_end_create(self, msg: str = "") -> None:
+        if msg:
+            QMessageBox(QMessageBox.Icon.Critical, "Error", msg, QMessageBox.StandardButton.Ok | QMessageBox.StandardButton.Cancel).exec_()
+        else:
+            QMessageBox(QMessageBox.Icon.Information, "Info", "Created successfully!", QMessageBox.StandardButton.Ok | QMessageBox.StandardButton.Cancel).exec_()
+        self.frame.hide()
+        self.btn_create.show()
+    
+    def closeEvent(self, event: QCloseEvent) -> None:
+        if self._thread and self._thread.isRunning():
+            if QMessageBox(QMessageBox.Icon.Warning, "Warning", "App is running! Do you want to close?", QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No).exec_() == QMessageBox.StandardButton.No:
+                return event.ignore()
+            self._thread.stop()
+        self.__cache.update()
+        super().closeEvent(event)
 
 
 if __name__ == '__main__':
